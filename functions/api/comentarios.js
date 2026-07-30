9d7f99b39ddf05ee62defbaf52fb810f95a42e50
Bryan Thyago <bryanthyago99@gmail.com>
2026-07-30 20:30:57 -0300

    Update comentarios.js

--- functions/api/comentarios.js
+++ functions/api/comentarios.js
@@ -1,5 +1,8 @@
-export async function onRequest(context) {
-  const db = context.env.DB;
-  const { results } = await db.prepare("SELECT * FROM comentarios ORDER BY data DESC LIMIT 50").all();
-  return new Response(JSON.stringify(results), { headers: { 'Content-Type': 'application/json' } });
+export async function onRequest({env}) {
+  try {
+    const { results } = await env.DB.prepare("SELECT * FROM comentarios ORDER BY id DESC LIMIT 20").all();
+    return new Response(JSON.stringify(results), {headers: {'Content-Type': 'application/json'}});
+  } catch(e) {
+    return new Response(JSON.stringify({error: e.message}), {status: 500});
+  }
 }
