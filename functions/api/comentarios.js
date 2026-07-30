9e290c84766510b5a55b60dc9014eb3228d2290c
Bryan Thyago <bryanthyago99@gmail.com>
2026-07-30 19:34:41 -0300

    Delete functions/api/comentarios.js
    deletado

--- functions/api/comentarios.js
+++
@@ -1,9 +0,0 @@
-export async function onRequestGet(context) {
-  const { request, env } = context;
-  const url = new URL(request.url);
-  const shader_id = url.searchParams.get("shader_id");
-  const { results } = await env.DB.prepare(
-    "SELECT * FROM comentarios WHERE shader_id = ? ORDER BY data DESC"
-  ).bind(shader_id).all();
-  return Response.json(results);
-}
