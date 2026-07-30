427320c038a1487a1752d49da0a68d2949745b79
Bryan Thyago <bryanthyago99@gmail.com>
2026-07-30 19:54:50 -0300

    Update youtube.js

--- functions/api/youtube.js
+++ functions/api/youtube.js
@@ -1,32 +1,7 @@
-export async function onRequestGet(context) {
-  const { env } = context;
-  
-  const channelID = "UCtqAb6OTpfT-BRDgVp2aa3w";
-  const apiKey = env.YT_API_KEY;
-
-  if (!apiKey) {
-    return new Response(JSON.stringify({ error: "API Key não configurada" }), { status: 500 });
-  }
-
-  try {
-    // PEGANDO 12 VÍDEOS AGORA
-    const videosURL = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelID}&part=snippet&order=date&maxResults=12&type=video`;
-    
-    const liveURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelID}&eventType=live&type=video&key=${apiKey}`;
-
-    const [videosRes, liveRes] = await Promise.all([fetch(videosURL), fetch(liveURL)]);
-    
-    const videosData = await videosRes.json();
-    const liveData = await liveRes.json();
-
-    return new Response(JSON.stringify({
-      videos: videosData,
-      live: liveData
-    }), {
-      headers: { "Content-Type": "application/json" }
-    });
-
-  } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), { status: 500 });
-  }
+export async function onRequest(context) {
+  const YT_API_KEY = context.env.YT_API_KEY;
+  const CHANNEL_ID = 'UCtqAb6OTpfT-BRDgVp2aa3w';
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${YT_API_KEY}&channelId=${CHANNEL_ID}&part=snippet&order=date&maxResults=6&type=video`;
+  const res = await fetch(url);
+  return new Response(await res.text(), { headers: { 'Content-Type': 'application/json' } });
 }
