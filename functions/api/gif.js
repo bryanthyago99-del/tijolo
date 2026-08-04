112f96eddfbbe5433518a633a72373b3cd97953d
Bryan Thyago <bryanthyago99@gmail.com>
2026-08-04 17:42:06 -0300

    Delete functions/api directory

--- functions/api/gif.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context){
-  const {request, env} = context;
-  const q = new URL(request.url).searchParams.get('q');
-  const KEY = env.GIPHY_KEY;
-  const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${KEY}&q=${q}&limit=10`);
-  return new Response(await res.text(), {headers:{'Content-Type':'application/json'}});
-}
--- functions/api/youtube.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context){
-  const {request, env} = context;
-  const type = new URL(request.url).searchParams.get('type');
-  const KEY = env.YT_API_KEY;
-  let url = `https://www.googleapis.com/youtube/v3/search?key=${KEY}&channelId=UCtqAb6OTpfT-BRDgVp2aa3w&part=snippet&order=date&maxResults=12&type=video`;
-  if(type==='short') url += '&videoDuration=short';
-  const res = await fetch(url);
-  return new Response(await res.text(), {headers:{'Content-Type':'application/json'}});
-}
